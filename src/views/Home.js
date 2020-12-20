77c3ba21f8066ee3b45c39ea9e40997f87630eac
ludmiladev <45207186+ludmiladev@users.noreply.github.com>
2020-12-20 20:50:11 +0200

    добавила src

---
+++ src/views/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Home = () => (
+  <div style={{ width: '700px' }}>
+    <ul>
+      <h2>Phonebook Homepage</h2>
+      <li>Keep your contacts organized and up to date.</li>
+      <li>Find your contacts by name.</li>
+      <li>Easily add contacts and edit information.</li>
+      <li>
+        Get suggestions for adding new contacts, merging duplicates, and more
+      </li>
+    </ul>
+    <img
+      src="https://st2.depositphotos.com/4431055/11471/i/600/depositphotos_114717032-stock-photo-old-fashioned-phone.jpg"
+      alt="img"
+      width="700"
+    />
+  </div>
+);
+
+export default Home;
